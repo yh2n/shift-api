b4cea06f175c99e94a2f241d1ee34d6b4f43a70a
yh2n <yohann.potico@gmail.com>
2019-03-31 19:59:02 -0400

    Fix .gitignore

--- tests/server.test.js
+++ tests/server.test.js
@@ -2,7 +2,7 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const should = chai.should();
 const {app, runServer, closeServer} = require('../server');
-const {TEST_DATABASE_URL} = require('../config');
+const {TEST_DATABASE_URL} = require('../config/keys');
 
 chai.use(chaiHttp);
 
