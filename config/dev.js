ae6f41554f66df571cbbc313b71460c26ab992f6
yh2n <yohann.potico@gmail.com>
2020-09-07 03:01:05 -0400

    Connect to new db after migration to mongo atlas

--- config/dev.js
+++ config/dev.js
@@ -1,14 +1,12 @@
-exports.DATABASE_URL = process.env.DATABASE_URL || 
-					   global.DATABASE_URL || 
-					//   'mongodb://tenka:satya@ds113063.mlab.com:13063/staff-data';
-
-					'mongodb://tenka:satya0517@ds239337-a0.mlab.com:39337,ds239337-a1.mlab.com:39337/shift_db_dev?replicaSet=rs-ds239337'
-					exports.TEST_DATABASE_URL = (process.env.TEST_DATABASE_URL ||
-						'mongodb://localhost/test-staff-data');
+exports.DATABASE_URL =
+  process.env.DATABASE_URL ||
+  global.DATABASE_URL ||
+  "mongodb+srv://tenka:satya17@cluster0.0mvko.mongodb.net/shift_db?retryWrites=true&w=majority";
+exports.TEST_DATABASE_URL =
+  process.env.TEST_DATABASE_URL || "mongodb://localhost/test-staff-data";
 exports.PORT = process.env.PORT || 8080;
 
-exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
+exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || "http://localhost:3000";
 
-exports.JWT_SECRET = process.env.JWT_SECRET || 'WHITE_RABBIT';
-exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
-
+exports.JWT_SECRET = process.env.JWT_SECRET || "WHITE_RABBIT";
+exports.JWT_EXPIRY = process.env.JWT_EXPIRY || "7d";
